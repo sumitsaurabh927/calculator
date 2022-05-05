aa89d1655fabc1baca4811fa9dc6fdc154d6dddd
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-05 15:14:45 +0530

    js: add basic mathematical operations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 // testing JS wiring
 // console.log('wired');
 let numbers = document.querySelectorAll('.num');
+
+// functions for four basic numberical operations
+let addition = (operandOne, operandTwo) => operandOne + operandTwo;
+let subtraction = (operandOne, operandTwo) => operandOne - operandTwo;
+let multiplication = (operandOne, operandTwo) => operandOne * operandTwo;
+let division = (operandOne, operandTwo) => operandOne / operandTwo;
