c4734cd7dc4100c05f49e1c5d6c3518df75b5942
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-05 17:59:09 +0530

    js: add function to populate numbers on calculator screen

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 // testing JS wiring
 // console.log('wired');
 let numbers = document.querySelectorAll('.num');
+let zero = numbers[0];
+let one = numbers[1];
+let two = numbers[2];
+let three = numbers[3];
+let four = numbers[4];
+let five = numbers[5];
+let six = numbers[6];
+let seven = numbers[7];
+let eight = numbers[8];
+let nine = numbers[9];
+
+let screen = document.querySelector('.input');
 
 // functions for four basic numberical operations
 let addition = (operandOne, operandTwo) => operandOne + operandTwo;
@@ -19,7 +31,29 @@ let operate = (operator, operandOne, operandTwo) => {
                 return multiplication(operandOne, operandTwo);
         } else if (operator === '/') {
                 return division(operandOne, operandTwo);
-        }else{
+        } else {
                 console.log('no more operators')
         }
 }
+
+// populating screen on button press
+let inputValue = screen.innerText;
+const populator = (clickVal) => {
+        let clicked = clickVal.innerText;
+        inputValue = inputValue + clicked;
+        // console.log(inputValue);
+        screen.innerText = inputValue;
+}
+
+one.addEventListener('click', () => populator(one));
+two.addEventListener('click', () => populator(two));
+three.addEventListener('click', () => populator(three));
+four.addEventListener('click', () => populator(four));
+five.addEventListener('click', () => populator(five));
+six.addEventListener('click', () => populator(six));
+seven.addEventListener('click', () => populator(seven));
+eight.addEventListener('click', () => populator(eight));
+nine.addEventListener('click', () => populator(nine));
+
+
+
