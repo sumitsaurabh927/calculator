931f8a14fdbafd9340c791970a7ff00912837b84
Sumit Saurabh <sumitsaurabh927@gmail.com>
2022-05-05 15:46:29 +0530

    js: add operate function

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ let addition = (operandOne, operandTwo) => operandOne + operandTwo;
 let subtraction = (operandOne, operandTwo) => operandOne - operandTwo;
 let multiplication = (operandOne, operandTwo) => operandOne * operandTwo;
 let division = (operandOne, operandTwo) => operandOne / operandTwo;
+
+// the operate function
+let operate = (operator, operandOne, operandTwo) => {
+        // console.log(operator,typeof(operator))
+        if (operator === '+') {
+                return addition(operandOne, operandTwo);
+        } else if (operator === '-') {
+                return subtraction(operandOne, operandTwo);
+        } else if (operator === '*') {
+                return multiplication(operandOne, operandTwo);
+        } else if (operator === '/') {
+                return division(operandOne, operandTwo);
+        }else{
+                console.log('no more operators')
+        }
+}
