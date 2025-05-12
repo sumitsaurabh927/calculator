ea8bed8d9739322c5f4ba1f8a73c1525bfe689f7
Sumit Saurabh <sumitsaurabh927@gmail.com>
2025-05-12 20:49:47 +0530

    api: add auth

--- js/main.js
+++ js/main.js
@@ -268,3 +268,4 @@ const severalOperatorsCheck = (inputString) => {
 console.log('this is a test')
 console.log('this is another test')
 console.log('this is another test')
+console.log('this is another test')
